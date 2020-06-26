8e96c7af409923894300b5f6495d7f35a8650517
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-06-25 21:17:39 -0400

    WPF ComboBox Control Demo
    Ctreating Class Library.
    Added more properties to the PersonModel and adding AddressModel. Add DataAcces and populated with fake data. Using ComboBox to show random people and thier random addresses.

---
+++ DemoLibrary/DataAccess.cs
@@ -0,0 +1,87 @@
+﻿using DemoLibrary.Models;
+using System;
+using System.Collections.Generic;
+
+namespace DemoLibrary
+{
+    public class DataAccess
+    {
+        private Random rnd = new Random();
+        private string[] streetAddresses = new string[] { "101 State Street", "425 Oak Avenue", "7 Wallace Way", "928 Edwards Court", "29 Main Avenue" };
+        private string[] cities = new string[] { "Springfield", "Wilshire", "Alexandria", "Franklin", "Clinton", "Fairview", "Madison" };
+        private string[] states = new string[] { "WI", "GA", "PA", "TX", "CA", "IL", "WA", "VA", "FL", "OK", "AZ" };
+        private string[] zipCodes = new string[] { "14121", "08904", "84732", "23410", "60095", "60618", "10456", "00926", "08701", "90280", "92335", "79936" };
+        private string[] firstNames = new string[] { "Bob", "Sue", "Carla", "Frank", "Monique", "Carlton", "Miguel", "Daniel", "Santiago", "John", "Robert" };
+        private string[] lastNames = new string[] { "Smith", "Jones", "Garcia", "Miller", "Thomas", "Lee", "Taylor", "Wilson", "Martinez", "Davis", "Hernandez" };
+        private bool[] aliveStatuses = new bool[] { true, false };
+        private DateTime lowEndDate = new DateTime(1943, 1, 1);
+        private int daysFromLowDate;
+
+        public DataAccess()
+        {
+            daysFromLowDate = (DateTime.Today - lowEndDate).Days;
+        }
+        public List<PersonModel> GetPeople(int total = 10)
+        {
+            List<PersonModel> output = new List<PersonModel>();
+            for (int i = 0; i < total; i++)
+            {
+                output.Add(GetPerson(i + 1));
+            }
+            return output;
+        }
+
+        private PersonModel GetPerson(int id)
+        {
+            PersonModel output = new PersonModel();
+
+            output.PersonId = id;
+            output.FirstName = GetRandomItem(firstNames);
+            output.LastName = GetRandomItem(lastNames);
+            output.IsAlive = GetRandomItem(aliveStatuses);
+            output.DateOfBirth = GetRandomDate();
+            output.Age = GetAgeInYears(output.DateOfBirth);
+            output.AccountBalance = ((decimal)rnd.Next(1, 1000000) / 100);
+
+            int addressCount = rnd.Next(1, 5);
+            for (int i = 0; i < addressCount; i++)
+            {
+                output.Addresses.Add(GetAddress(((id - 1) * 5) + i + 1));
+            }
+
+            return output;
+        }
+        private AddressModel GetAddress(int id)
+        {
+            AddressModel output = new AddressModel();
+
+            output.AddressId = id;
+            output.StreetAddress = GetRandomItem(streetAddresses);
+            output.City = GetRandomItem(cities);
+            output.State = GetRandomItem(states);
+            output.ZipCode = GetRandomItem(zipCodes);
+
+            return output;
+        }
+        private T GetRandomItem<T>(T[] data)
+        {
+            return data[rnd.Next(0, data.Length)];
+        }
+
+        private DateTime GetRandomDate()
+        {
+            return lowEndDate.AddDays(rnd.Next(daysFromLowDate));
+        }
+
+        private int GetAgeInYears(DateTime birthday)
+        {
+            DateTime now = DateTime.Today;
+            int age = now.Year - birthday.Year;
+            if (now < birthday.AddYears(age))
+            {
+                age--;
+            }
+            return age;
+        }
+    }
+}
--- WPFUI/ViewModels/ShellViewModel.cs
+++ WPFUI/ViewModels/ShellViewModel.cs
@@ -1,101 +1,154 @@
 ﻿using Caliburn.Micro;
-using WPFUI.Models;
+using DemoLibrary;
+using DemoLibrary.Models;
+using System.ComponentModel;
 
 namespace WPFUI.ViewModels
 {
-    public class ShellViewModel : Conductor<object>
+    public class ShellViewModel : Screen
     {
-        private string _firstName = "Iyad";
-        private string _lastName;
-        private BindableCollection<PersonModel> _people = new BindableCollection<PersonModel>();
+        public BindableCollection<PersonModel> People { get; set; }
+
         private PersonModel _selectedPerson;
-
-        public ShellViewModel()
-        {
-            People.Add(new PersonModel { FirstName = "Iyad", LastName = "Shobaki" });
-            People.Add(new PersonModel { FirstName = "Bill", LastName = "Jones" });
-            People.Add(new PersonModel { FirstName = "Sue", LastName = "Storm" });
-        }
-        public string FirstName
-        {
-            get
-            {
-                return _firstName;
-            }
-            set
-            {
-                _firstName = value;
-                NotifyOfPropertyChange(() => FirstName);
-                NotifyOfPropertyChange(() => FullName);
-            }
-        }
-
-        public string LastName
-        {
-            get
-            {
-                return _lastName;
-            }
-            set
-            {
-                _lastName = value;
-                NotifyOfPropertyChange(() => LastName);
-                NotifyOfPropertyChange(() => FullName);
-            }
-        }
-
-        public string FullName
-        {
-            get
-            {
-                return $"{ FirstName } { LastName }";
-            }
-        }
-
-        public BindableCollection<PersonModel> People
-        {
-            get { return _people; }
-            set { _people = value; }
-        }
 
         public PersonModel SelectedPerson
         {
             get { return _selectedPerson; }
             set
             {
+              
                 _selectedPerson = value;
+                SelectedAddress = value.PrimaryAddress;
                 NotifyOfPropertyChange(() => SelectedPerson);
+
             }
         }
 
-        public bool CanClearText(string firstName,
-            string lastName)// => !string.IsNullOrWhiteSpace(firstName) || !string.IsNullOrWhiteSpace(lastName);
+        private AddressModel _selectedAddress;
+
+        public AddressModel SelectedAddress
         {
-            //return !string.IsNullOrWhiteSpace(firstName) || !string.IsNullOrWhiteSpace(lastName);
-            if (string.IsNullOrWhiteSpace(firstName) && string.IsNullOrWhiteSpace(lastName))
+            get { return _selectedAddress; }
+            set
             {
-                return false;
+                _selectedAddress = value;
+                SelectedPerson.PrimaryAddress = value;
+                NotifyOfPropertyChange(() => SelectedAddress);
+
             }
-            else
-            {
-                return true;
-            }
-        }
-        public void ClearText(string firstName, string lastName)
-        {
-            FirstName = "";
-            LastName = "";
         }
 
-        public void LoadPageOne()
+        public ShellViewModel()
         {
-            ActivateItem( new FirstChildViewModel());
+            DataAccess da = new DataAccess();
+            People = new BindableCollection<PersonModel>(da.GetPeople());
         }
-
-        public void LoadPageTwo()
-        {
-            ActivateItem(new SecondChildViewModel());
-        }
-
     }
 }
+
+
+
+
+
+//old code 
+//using Caliburn.Micro;
+//using WPFUI.Models;
+
+//namespace WPFUI.ViewModels
+//{
+//    public class ShellViewModel : Conductor<object>
+//    {
+//        private string _firstName = "Iyad";
+//        private string _lastName;
+//        private BindableCollection<PersonModel> _people = new BindableCollection<PersonModel>();
+//        private PersonModel _selectedPerson;
+
+//        public ShellViewModel()
+//        {
+//            People.Add(new PersonModel { FirstName = "Iyad", LastName = "Shobaki" });
+//            People.Add(new PersonModel { FirstName = "Bill", LastName = "Jones" });
+//            People.Add(new PersonModel { FirstName = "Sue", LastName = "Storm" });
+//        }
+//        public string FirstName
+//        {
+//            get
+//            {
+//                return _firstName;
+//            }
+//            set
+//            {
+//                _firstName = value;
+//                NotifyOfPropertyChange(() => FirstName);
+//                NotifyOfPropertyChange(() => FullName);
+//            }
+//        }
+
+//        public string LastName
+//        {
+//            get
+//            {
+//                return _lastName;
+//            }
+//            set
+//            {
+//                _lastName = value;
+//                NotifyOfPropertyChange(() => LastName);
+//                NotifyOfPropertyChange(() => FullName);
+//            }
+//        }
+
+//        public string FullName
+//        {
+//            get
+//            {
+//                return $"{ FirstName } { LastName }";
+//            }
+//        }
+
+//        public BindableCollection<PersonModel> People
+//        {
+//            get { return _people; }
+//            set { _people = value; }
+//        }
+
+//        public PersonModel SelectedPerson
+//        {
+//            get { return _selectedPerson; }
+//            set
+//            {
+//                _selectedPerson = value;
+//                NotifyOfPropertyChange(() => SelectedPerson);
+//            }
+//        }
+
+//        public bool CanClearText(string firstName,
+//            string lastName)// => !string.IsNullOrWhiteSpace(firstName) || !string.IsNullOrWhiteSpace(lastName);
+//        {
+//            //return !string.IsNullOrWhiteSpace(firstName) || !string.IsNullOrWhiteSpace(lastName);
+//            if (string.IsNullOrWhiteSpace(firstName) && string.IsNullOrWhiteSpace(lastName))
+//            {
+//                return false;
+//            }
+//            else
+//            {
+//                return true;
+//            }
+//        }
+//        public void ClearText(string firstName, string lastName)
+//        {
+//            FirstName = "";
+//            LastName = "";
+//        }
+
+//        public void LoadPageOne()
+//        {
+//            ActivateItem( new FirstChildViewModel());
+//        }
+
+//        public void LoadPageTwo()
+//        {
+//            ActivateItem(new SecondChildViewModel());
+//        }
+
+//    }
+//}
